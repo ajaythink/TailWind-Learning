0a0442e2ad8a78e6e1fcfad076298497639e8f4e
Ajay Kumar <104864110+ajaythink@users.noreply.github.com>
2024-06-15 22:26:22 +0530

    Tailwind Leaning daily updates

---
+++ First1/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+//  content: ["dist/index.html"], -> this line only for single html file
+ content: ["dist/*.html"],   //this will be multiple html files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
